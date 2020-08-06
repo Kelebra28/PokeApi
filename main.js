9b524261bc58b49db68dbcdc2a5e1a7eb4368490
Kelebra <ricardokelebra@gmail.com>
2020-08-06 18:09:11 -0500

    style pokemon

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ const makePokemon = (pokeData) => {
 
     let pokeContainer = document.createElement('div')
     pokeContainer.classList.add('poke-container')
+    pokeContainer.classList.add('animate__fadeOutDown')
 
     let pokeContainerIMG = document.createElement('div')
     pokeContainerIMG.classList.add('img-poke')
